b0bbf3c6b00fb0e31c595c919a39d9fafd68332c
Andy Chen <chenyangpoppin@hotmail.com>
2021-01-02 16:30:10 +0800

    refactor: rename variable & fix comment thing

--- simple-tapable.js
+++ simple-tapable.js
@@ -2,14 +2,14 @@
 
 const { SyncHook } = require('tapable');
 
-const hook1 = new SyncHook(['arg1', 'arg2', 'arg3']);
+const hook = new SyncHook(['arg1', 'arg2', 'arg3']);
 
 // bind sync events
-// in webpack, we use similar strategies to bind & execute events
-hook1.tap('sync', (arg1, arg2, arg3) => {
+// In webpack, we use similar strategies to bind & execute events
+hook.tap('sync', (arg1, arg2, arg3) => {
   console.log('pint out sync hook args: ');
   console.log(arg1, arg2, arg3); // 4, 5, 6
 });
 
 // execute sync events
-hook1.call(4, 5, 6);
+hook.call(4, 5, 6);
